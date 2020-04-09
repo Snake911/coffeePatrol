549f0c24dbc2fbae917f4db96509228a4d6c4645
Snake911 <snake1234567890-1@yandex.ru>
2020-04-09 13:54:05 +0500

    Delete keys.js

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://snake:123456dv@danil-shard-00-00-iduby.mongodb.net:27017,danil-shard-00-01-iduby.mongodb.net:27017,danil-shard-00-02-iduby.mongodb.net:27017/test?ssl=true&replicaSet=Danil-shard-0&authSource=admin&retryWrites=true',
-    jwt: 'dev-jwt' 
-}
